fb4c75aa655004e0c6f9805b21c98f8420a5c28e
Rui-z817 <924237268@qq.com>
2026-07-27 12:02:30 +0800

    update: set base path to root for lighthouse

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/neicai-procurement-platform/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
